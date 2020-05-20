b651333ebb32a9dc2d20e964b2fd7a13e68fa94b
Lichao Wang <wanglichao.1121@163.com>
2020-05-20 19:44:16 +0800

    first commit

---
+++ precache-manifest.e9f6ef399bb58ec3dbc7c92b88ac65ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6308e60332e4a65b256b2c78ab868e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b04fc573c9125165a86f",
+    "url": "/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "dd7be9446071efdc5644",
+    "url": "/static/js/2.402ba365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.402ba365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b04fc573c9125165a86f",
+    "url": "/static/js/main.d8a7fb75.chunk.js"
+  },
+  {
+    "revision": "688399a6d1c78c1529ed",
+    "url": "/static/js/runtime-main.19d05d83.js"
+  },
+  {
+    "revision": "613cdea6e99523564ad481490f3c449e",
+    "url": "/static/media/bg.613cdea6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fc1f606dc5a542386e5d679bd71d94fd",
+    "url": "/static/media/setting.fc1f606d.jpg"
+  },
+  {
+    "revision": "97a7c1ea88e76d36aff12daa4e4e90b7",
+    "url": "/static/media/temp.97a7c1ea.jpg"
+  }
+]);
